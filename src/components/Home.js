9fa43b5840674c104c14911d601068ad138c185e
Jesus Aranda <aranda.jm@icloud.com>
2021-03-15 18:04:00 -0300

    fetch new releases from spotify api and show them into the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [newReleases, setNewReleases] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(
+        "https://api.spotify.com/v1/browse/new-releases?limit=20",
+        {
+          method: "GET",
+          headers: {
+            Authorization:
+              "Bearer BQDMxqCvWidIsq4H1yml_V2Wqjuy8B4aZfCuT-hx82T8yuaNoF_4iCjHNvpAr-E8Coja2umhTzsrU9bLOAjnUDT5ZEH1Gw25O-zGcEfZcicEYd64XHtkZmGC0pCeHagQU2V-aaewqkjRo2OlhOU",
+          },
+        }
+      );
+      setNewReleases(res.data.albums.items);
+    })();
+  }, []);
+
+  console.log(newReleases);
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-    </div>
+    <React.Fragment>
+      <div className="card-columns m-5 animated fadeIn">
+        {newReleases &&
+          newReleases.map((item) => (
+            <div className="card puntero" key={item.uri}>
+              <img
+                src={item.images[0].url}
+                className="card-img-top"
+                alt="item.name"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{item.name}</h5>
+                {item.artists &&
+                  item.artists.map((artist) => (
+                    <p className="card-text" key={artist.name}>
+                      <span className="badge badge-pill badge-primary">
+                        {artist.name}
+                      </span>
+                    </p>
+                  ))}
+              </div>
+            </div>
+          ))}
+      </div>
+    </React.Fragment>
   );
 };
 
